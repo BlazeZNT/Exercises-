58c99ead9d35198ff3441a0c74458e470e1826e1
Zar Ni Tun <zarnitunkt@gmail.com>
2023-12-10 21:06:09 +0630

    Initialize and finish

---
+++ SubUnit16/Script.js
@@ -0,0 +1,11 @@
+// var PI = 3.14;
+// PI = 42;
+
+const PI = 3.14;
+
+console.log(hehe);
+
+// 1. var has function scope while let has block scope
+// 2. Same as above but also var is mutable but const is not
+// 3. let is mutalbe while const is not .
+// 4. hosting occurs in var when even if a variable that has yet to be defined is run instead of showing an error it will say undefined since the varaible is hoisted up with an undefined value, although it will be defined later on.
